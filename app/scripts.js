ed85fdfd95b4eb010814b489f39226ef2c0a6ae8
Carolyn Selheim <carolyn.selheim@gmail.com>
2018-01-10 14:16:33 -0700

    created new function

--- app/scripts.js
+++ app/scripts.js
@@ -1,11 +1,27 @@
 var arrayOfObjects = [
-	{id: 1,
-	 name: Brian,
- 	 age: 37},
-	 {id: 2,
- 	 name: Carolyn,
-  	 age: 29},
-	 {id: 3,
- 	 name: Lucy,
-  	 age: 9},
-]
+  {
+    id: 1,
+    name: Brian,
+    age: 37
+  },
+  {
+    id: 2,
+    name: Carolyn,
+    age: 29
+  },
+  {
+    id: 3,
+    name: Lucy,
+    age: 9
+  }
+];
+
+var carolynsNumbers = [1, 2, 3];
+
+function multiplyNumbers(array) {
+  var answer;
+  for (var i = 0; i < array.length; i++) {
+    answer *= array[i];
+  }
+  return answer;
+}
